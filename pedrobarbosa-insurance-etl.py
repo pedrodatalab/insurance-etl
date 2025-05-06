c4b5bba6f4c6041710fb315a7e40f57269183dd5
Pedro Barbosa <datapedrobarbosa@gmail.com>
2025-05-06 20:07:02 -0300

    atualização do script python

--- pedrobarbosa-insurance-etl.py
+++ pedrobarbosa-insurance-etl.py
@@ -1,5 +1,12 @@
-#Import das lib
+#Import librarys
 
 import pandas as pd
 import datetime
 import fastparquet 
+import os 
+
+policies = 
+claims = 
+
+df_policies = pd.policies
+df_claims = pd.claims
